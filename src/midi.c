6a1e1796af7615bb61ea080a71e6c71206b77cdd
rhargreaves <robert@roberthargreaves.net>
2020-10-11 20:15:46 +0100

    Add MIDI emit code

--- src/midi.c
+++ src/midi.c
@@ -1,3 +1,11 @@
 #include "midi.h"
+#include <genesis.h>
 
-void midi_emit(u8 midiByte);
+static u16 y = 10;
+
+void midi_emit(u8 midiByte)
+{
+    char text[32];
+    sprintf(text, "MIDI emit: 0x%x", midiByte);
+    VDP_drawText(text, 1, y++);
+}
