5cca9bd5e1bae53a16800d570705599db3c10d04
rhargreaves <robert@roberthargreaves.net>
2020-10-11 20:17:22 +0100

    Add wrap for midi_emit

--- tests/wraps.c
+++ tests/wraps.c
@@ -10,3 +10,8 @@ int _eflash;
 void __wrap_VDP_drawText(const char* str, u16 x, u16 y)
 {
 }
+
+void __wrap_midi_emit(u8 midiByte)
+{
+    check_expected(midiByte);
+}
--- tests/wraps.h
+++ tests/wraps.h
@@ -3,3 +3,4 @@
 #include <types.h>
 
 void __wrap_VDP_drawText(const char* str, u16 x, u16 y);
+void __wrap_midi_emit(u8 midiByte);
