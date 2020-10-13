101f24de1a0c3e87c1f27d96e3f802a7b21537b0
rhargreaves <robert@roberthargreaves.net>
2020-10-13 22:22:06 +0100

    Add missing header

--- tests/wraps.h
+++ tests/wraps.h
@@ -7,3 +7,4 @@ void __wrap_VDP_drawText(const char* str, u16 x, u16 y);
 void __wrap_midi_emit(u8 midiByte);
 mw_err __wrap_mediator_recv_event(void);
 mw_err __wrap_mediator_send_packet(u8 ch, char* data, u16 len);
+void __wrap_SYS_die(char* err);
