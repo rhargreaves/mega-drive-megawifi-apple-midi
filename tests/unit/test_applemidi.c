3b1eea146825654b8c6a040492985f1e470e784c
rhargreaves <robert@roberthargreaves.net>
2020-10-13 22:43:55 +0100

    Test more 2 byte midi events

--- tests/unit/test_applemidi.c
+++ tests/unit/test_applemidi.c
@@ -35,18 +35,22 @@ static void test_applemidi_parses_rtpmidi_packet_with_single_midi_event(
 static void test_applemidi_parses_rtpmidi_packet_with_single_2_byte_midi_event(
     UNUSED void** state)
 {
-    char rtp_packet[1024]
-        = { /* V P X CC M PT */ 0x80, 0x61, /* sequence number */ 0x8c, 0x24,
-              /* timestamp */ 0x00, 0x58, 0xbb, 0x40, /* SSRC */ 0xac, 0x67,
-              0xe1, 0x08, /* MIDI command section */ 0x02, 0xC0, 0x01 };
+    const u8 statuses[] = { 0xC0, 0xD0 };
 
-    size_t len = sizeof(rtp_packet);
+    for (u16 i = 0; i < sizeof(statuses); i++) {
+        u8 status = statuses[i];
+        char rtp_packet[1024] = { /* V P X CC M PT */ 0x80, 0x61,
+            /* sequence number */ 0x8c, 0x24,
+            /* timestamp */ 0x00, 0x58, 0xbb, 0x40, /* SSRC */ 0xac, 0x67, 0xe1,
+            0x08, /* MIDI command section */ 0x02, status, 0x01 };
+        size_t len = sizeof(rtp_packet);
 
-    expect_midi_emit(0xC0);
-    expect_midi_emit(0x01);
+        expect_midi_emit(status);
+        expect_midi_emit(0x01);
 
-    mw_err err = applemidi_processSessionMidiPacket(rtp_packet, len);
-    assert_int_equal(err, MW_ERR_NONE);
+        mw_err err = applemidi_processSessionMidiPacket(rtp_packet, len);
+        assert_int_equal(err, MW_ERR_NONE);
+    }
 }
 
 static void
@@ -82,6 +86,7 @@ static void test_applemidi_parses_rtpmidi_packet_with_two_midi_events(
     expect_midi_emit(0x90);
     expect_midi_emit(0x48);
     expect_midi_emit(0x6f);
+
     expect_midi_emit(0x90);
     expect_midi_emit(0x51);
     expect_midi_emit(0x6f);
@@ -104,9 +109,11 @@ static void test_applemidi_parses_rtpmidi_packet_with_multiple_midi_events(
     expect_midi_emit(0x90);
     expect_midi_emit(0x48);
     expect_midi_emit(0x6f);
+
     expect_midi_emit(0x90);
     expect_midi_emit(0x51);
     expect_midi_emit(0x6f);
+
     expect_midi_emit(0x90);
     expect_midi_emit(0x48);
     expect_midi_emit(0x6f);
@@ -130,9 +137,11 @@ test_applemidi_parses_rtpmidi_packet_with_multiple_different_midi_events(
     expect_midi_emit(0x90);
     expect_midi_emit(0x48);
     expect_midi_emit(0x6f);
+
     expect_midi_emit(0x90);
     expect_midi_emit(0x51);
     expect_midi_emit(0x6f);
+
     expect_midi_emit(0x80);
     expect_midi_emit(0x48);
     expect_midi_emit(0x6f);
